05ff87f39f19f4ac9e5cc846a3a43a88e0a945f9
Kevin dan mathew <lee777601@gmail.com>
2026-02-17 21:20:33 +0530

    Add wrapper function declaration for mir_generate_asm

--- include/mir_gen.h
+++ include/mir_gen.h
@@ -1,14 +1,2 @@
-#ifndef MIR_GEN_H
-#define MIR_GEN_H
-
-#include "ast.h"
-
-/*
- * MIR (Micro Intermediate Representation) generator
- * Converts AST to x86-64 assembly
- */
-
-// Generate MIR/Assembly from AST root
-void mir_generate(ASTNode *root, const char *asm_filename);
-
-#endif
+// Wrapper function declaration for mir_generate_asm
+void mir_generate_asm();
